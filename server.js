5678af8e22abcd728539e645380f5fb1e9a7005a
chikechris <cchike2017@gmail.com>
2019-12-16 18:20:29 -0500

    working on routes

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express()
 
 app.get('/', (req, res)=> res.json({msg: `welcome`}))
 
+// route definations
+app.use('/api/users', require('./routes/users'))
+app.use('/api/employees', require('./routes/employees'))
+app.use('/api/auth', require('./routes/auth'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> console.log(`server on port${PORT}`))
